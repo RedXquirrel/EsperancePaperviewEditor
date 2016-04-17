7a50fdce5f12b640b264a2817bff97d4c761eab7
Xamtastic <anthony.harrison@xamtastic.com>
2016-04-17 21:13:12 +0100

    [WIP] ManagePublishersViewModel added.

---
+++ Chat.Esperance.PaperviewApi/ViewModels/ManagePublishersViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Chat.Esperance.PaperviewApi.ViewModels
+{
+    public class ManagePublishersViewModel : ViewModelBase
+    {
+        private string _title = $"Manage Publishers View Model Text on idiom {Device.Idiom.ToString()}";
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; RaisePropertyChanged(); }
+        }
+    }
+}
