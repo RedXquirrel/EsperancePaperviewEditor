75769e64bc25c6967054c1d3afa33917e9585f74
Xamtastic <anthony.harrison@xamtastic.com>
2016-04-22 13:26:32 +0100

    [WIP] Tablet idiom master-detail presenting with full index, responses to taps not yet in place.

--- Chat.Esperance.Paperview/Chat.Esperance.Paperview/Pages/MainMasterDetail/MasterTabletPage.xaml.cs
+++ Chat.Esperance.Paperview/Chat.Esperance.Paperview/Pages/MainMasterDetail/MasterTabletPage.xaml.cs
@@ -3,16 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Chat.Esperance.Paperview.Services;
 using Xamarin.Forms;
 
 namespace Chat.Esperance.Paperview.Pages.MainMasterDetail
 {
     public partial class MasterTabletPage : ContentPage
     {
+        public ListView ListView { get { return listView; } }
+
         public MasterTabletPage()
         {
-            InitializeComponent();
+            try
+            {
+                InitializeComponent();
+
+                NavigationPage.SetHasNavigationBar(this, false);
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            ListView.ItemsSource = MasterPageIndexService.Index;
         }
     }
 }
